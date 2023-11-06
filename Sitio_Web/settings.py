2b96ea1e4d68e116f4ec2a173feebd5e490ff363
Juan Díaz <jchendiaz15@gmail.com>
2023-11-05 23:16:48 -0600

    config

--- Sitio_Web/settings.py
+++ Sitio_Web/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-bal_@+_wse7lcr&vu^#jk7zvzp_%#vs@gxruu!!f+61n*6#2x7"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".vercel.app", ".now.sh", "127.0.0.1", "localhost"]
 
